0ae5a8c55e320e9edcf11126d7c62a89942816e0
Nicole Lee <njlee@stanford.edu>
2024-05-17 14:48:17 -0700

    gallery images

--- src/components/DemoGallery.js
+++ src/components/DemoGallery.js
@@ -6,6 +6,24 @@ import middleTop from "../images/demo-gallery/middle-top.png";
 import middleBottom from "../images/demo-gallery/middle-bottom.png";
 import rightTop from "../images/demo-gallery/right-top.png";
 import rightBottom from "../images/demo-gallery/right-bottom.png";
+import left1 from "../images/demo-gallery/left-1.png";
+import left2 from "../images/demo-gallery/left-2.png";
+import left3 from "../images/demo-gallery/left-3.png";
+import left4 from "../images/demo-gallery/left-4.png";
+import left5 from "../images/demo-gallery/left-5.png";
+
+import mid1 from "../images/demo-gallery/mid-1.png";
+import mid2 from "../images/demo-gallery/mid-2.png";
+import mid3 from "../images/demo-gallery/mid-3.png";
+import mid4 from "../images/demo-gallery/mid-4.png";
+import mid5 from "../images/demo-gallery/mid-5.png";
+import mid6 from "../images/demo-gallery/mid-6.png";
+
+import right1 from "../images/demo-gallery/right-1.png";
+import right2 from "../images/demo-gallery/right-2.png";
+import right3 from "../images/demo-gallery/right-3.png";
+import right4 from "../images/demo-gallery/right-4.png";
+import right5 from "../images/demo-gallery/right-5.png";
 
 import "react-multi-carousel/lib/styles.css";
 import "slick-carousel/slick/slick.css";
@@ -16,16 +34,26 @@ const DemoGallery = () => {
   return (
     <div className="overall-image-holder">
       <div className="vertical-image-holder">
-        <img src={leftTop} alt="rightMacbook" className="top-image" />
-        <img src={leftBottom} alt="rightMacbook" />
+        <img src={left1} alt="rightMacbook" className="top-image" />
+        <img src={left2} alt="rightMacbook" />
+        <img src={left3} alt="rightMacbook" />
+        <img src={left4} alt="rightMacbook" />
+        <img src={left5} alt="rightMacbook" />
       </div>
       <div className="vertical-image-holder">
-        <img src={middleTop} alt="rightMacbook" className="top-image" />
-        <img src={middleBottom} alt="rightMacbook" />
+        <img src={mid1} alt="rightMacbook" className="top-image" />
+        <img src={mid2} alt="rightMacbook" />
+        <img src={mid3} alt="rightMacbook" />
+        <img src={mid4} alt="rightMacbook" />
+        <img src={mid5} alt="rightMacbook" />
+        <img src={mid6} alt="rightMacbook" />
       </div>
       <div className="vertical-image-holder">
-        <img src={rightTop} alt="rightMacbook" className="top-image" />
-        <img src={rightBottom} alt="rightMacbook" />
+        <img src={right1} alt="rightMacbook" className="top-image" />
+        <img src={right2} alt="rightMacbook" />
+        <img src={right3} alt="rightMacbook" />
+        <img src={right4} alt="rightMacbook" />
+        <img src={right5} alt="rightMacbook" />
       </div>
     </div>
   );
