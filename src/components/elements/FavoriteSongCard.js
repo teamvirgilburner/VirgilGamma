f2d34869199e2985d63a848a8091a5d4a9286d7d
Nicole Lee <njlee@stanford.edu>
2024-05-17 14:30:32 -0700

    audio button stuff

--- src/components/elements/FavoriteSongCard.js
+++ src/components/elements/FavoriteSongCard.js
@@ -6,11 +6,24 @@ import PauseButton from "../../images/pause-button.png";
 function FavoriteSongCard({ imageUrl, heading, subheading, audioUrl }) {
   const audioRef = useRef(null);
   const [musicPlaying, setMusicPlaying] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handlePlay = () => {
-    if (audioRef.current) {
-      audioRef.current.play();
-      setMusicPlaying(true);
+  const handlePlayPause = async () => {
+    if (audioUrl && audioRef.current) {
+      if (musicPlaying) {
+        audioRef.current.pause();
+        setMusicPlaying(false);
+      } else {
+        setIsLoading(true);
+        try {
+          await audioRef.current.play();
+          setMusicPlaying(true);
+        } catch (error) {
+          console.error("Error playing audio:", error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
     }
   };
 
@@ -21,13 +34,14 @@ function FavoriteSongCard({ imageUrl, heading, subheading, audioUrl }) {
         <span className="song-card-title">{heading}</span>
         <span className="song-card-artist">{subheading}</span>
       </div>
-      <button className="play-button" onClick={handlePlay}>
-        {musicPlaying === true ? (
-          <img src={PauseButton} alt="playButton" className="song-card-image" />
-        ) : (
-          <img src={PlayButton} alt="playButton" className="song-card-image" />
-        )}
+      <button className="play-button" onClick={handlePlayPause}>
+        <img
+          src={musicPlaying ? PauseButton : PlayButton}
+          alt="playPauseButton"
+          className="play-pause-icon"
+        />
       </button>
+
       <audio ref={audioRef} src={audioUrl}></audio>
     </div>
   );
